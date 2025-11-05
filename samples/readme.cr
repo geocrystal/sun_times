b3e0820692ef27b539ed6715dee35f421783790a
Anton Maminov <anton.linux@gmail.com>
2025-11-05 18:54:52 +0200

    Update readme.cr

--- samples/readme.cr
+++ samples/readme.cr
@@ -2,6 +2,10 @@ require "../src/sun_times"
 
 # https://gml.noaa.gov/grad/solcalc/
 
+def fmt_time(t : Time?) : String
+  t ? t.to_s : "N/A"
+end
+
 def format_time_span(span : Time::Span)
   String.build do |s|
     s << "#{span.days}d " if span.days > 0
@@ -14,36 +18,38 @@ end
 sun = SunTimes::SunTime.new(49.8419, 24.0311) # Lviv
 location = Time::Location.load("Europe/Kyiv")
 date = Time.local
+# date = Time.local(2025, 6, 21, 0, 0, 0, location: location) # Summer Solstice
+# date = Time.local(2025, 12, 21, 0, 0, 0, location: location) # Winter Solstice
 
-astronomical_dawn_time = sun.astronomical_dawn(date, location)
-nautical_dawn_time = sun.nautical_dawn(date, location)
-civil_dawn_time = sun.civil_dawn(date, location)
-sunrise_time = sun.sunrise(date, location)
+astronomical_dawn_time = sun.astronomical_dawn?(date, location)
+nautical_dawn_time = sun.nautical_dawn?(date, location)
+civil_dawn_time = sun.civil_dawn?(date, location)
+sunrise_time = sun.sunrise?(date, location)
 solar_noon_time = sun.solar_noon(date, location)
-sunset_time = sun.sunset(date, location)
-civil_dusk_time = sun.civil_dusk(date, location)
-nautical_dusk_time = sun.nautical_dusk(date, location)
-astronomical_dusk_time = sun.astronomical_dusk(date, location)
+sunset_time = sun.sunset?(date, location)
+civil_dusk_time = sun.civil_dusk?(date, location)
+nautical_dusk_time = sun.nautical_dusk?(date, location)
+astronomical_dusk_time = sun.astronomical_dusk?(date, location)
 daylight_length = sun.daylight_length(date, location)
 
 puts "Timezone: #{location}"
 puts "Now:      #{date}"
 puts
 puts "=== Twilight Periods ==="
-puts "Astronomical dawn:  #{astronomical_dawn_time}"
-puts "Nautical dawn:      #{nautical_dawn_time}"
-puts "Civil dawn:         #{civil_dawn_time}"
-puts "Sunrise:            #{sunrise_time}"
-puts "Solar noon:         #{solar_noon_time}"
-puts "Sunset:             #{sunset_time}"
-puts "Civil dusk:         #{civil_dusk_time}"
-puts "Nautical dusk:      #{nautical_dusk_time}"
-puts "Astronomical dusk:  #{astronomical_dusk_time}"
+puts "Astronomical dawn:  #{fmt_time(astronomical_dawn_time)}"
+puts "Nautical dawn:      #{fmt_time(nautical_dawn_time)}"
+puts "Civil dawn:         #{fmt_time(civil_dawn_time)}"
+puts "Sunrise:            #{fmt_time(sunrise_time)}"
+puts "Solar noon:         #{fmt_time(solar_noon_time)}"
+puts "Sunset:             #{fmt_time(sunset_time)}"
+puts "Civil dusk:         #{fmt_time(civil_dusk_time)}"
+puts "Nautical dusk:      #{fmt_time(nautical_dusk_time)}"
+puts "Astronomical dusk:  #{fmt_time(astronomical_dusk_time)}"
 puts ""
 puts "=== Daylight ==="
 puts "Daylight:      #{format_time_span(daylight_length)}"
 
-if date < sunset_time
+if sunset_time && date < sunset_time
   daylight_left = sunset_time - date
   puts "Daylight left: #{format_time_span(daylight_left)}"
 end
