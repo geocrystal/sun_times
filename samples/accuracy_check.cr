91a8a49ff27826c7e0806055f352bd8644a784b9
Anton Maminov <anton.linux@gmail.com>
2025-11-21 17:09:42 +0200

    Update accuracy_check.cr

--- samples/accuracy_check.cr
+++ samples/accuracy_check.cr
@@ -29,15 +29,15 @@ def check_accuracy(
   puts
   puts "Calculated sunrise:    #{sunrise.to_s("%H:%M:%S")}"
   puts "Reference sunrise:     #{ref_sunrise.to_s("%H:%M:%S")}"
-  puts "Difference:            #{sunrise_diff.seconds} seconds"
+  puts "Difference:            #{sunrise_diff.total_seconds.to_i} seconds"
   puts
   puts "Calculated sunset:     #{sunset.to_s("%H:%M:%S")}"
   puts "Reference sunset:      #{ref_sunset.to_s("%H:%M:%S")}"
-  puts "Difference:            #{sunset_diff.seconds} seconds"
+  puts "Difference:            #{sunset_diff.total_seconds.to_i} seconds"
   puts
   puts "Calculated solar noon: #{noon.to_s("%H:%M:%S")}"
   puts "Reference solar noon:  #{ref_solar_noon.to_s("%H:%M:%S")}"
-  puts "Difference:            #{noon_diff.seconds} seconds"
+  puts "Difference:            #{noon_diff.total_seconds.to_i} seconds"
   puts "=" * 80
   puts
 end
@@ -96,4 +96,17 @@ check_accuracy(
   Time.local(2025, 11, 5, 5, 51, 0, location: sydney),
   Time.local(2025, 11, 5, 19, 27, 0, location: sydney),
   Time.local(2025, 11, 5, 12, 38, 39, location: sydney)
+)
+
+# 5. Lviv, Ukraine
+lviv = Time::Location.load("Europe/Kiev")
+date = Time.local(2025, 11, 21, location: lviv)
+check_accuracy(
+  "Lviv, Ukraine",
+  49.8419, 24.0311,
+  date,
+  lviv,
+  Time.local(2025, 11, 21, 7, 46, 0, location: lviv),
+  Time.local(2025, 11, 21, 16, 33, 0, location: lviv),
+  Time.local(2025, 11, 21, 12, 9, 38, location: lviv)
 )
